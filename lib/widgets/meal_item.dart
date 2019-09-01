79da7c5f511a89e5df997959f0b79bb4842b7771
ushair <ushairansari@gmail.com>
2019-09-01 18:35:21 +0530

    29

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -9,15 +9,15 @@ class MealItem extends StatelessWidget {
   final int duration;
   final Complexity complexity;
   final Affordability affordability;
-
+  final Function removeItem;
   MealItem(
-      {
-      @required this.id,
+      {@required this.id,
       @required this.title,
       @required this.imageUrl,
       @required this.affordability,
       @required this.complexity,
-      @required this.duration});
+      @required this.duration,
+      @required this.removeItem});
 
   String get complexityText {
     switch (complexity) {
@@ -52,10 +52,16 @@ class MealItem extends StatelessWidget {
   }
 
   void selectMeal(BuildContext context) {
-    Navigator.of(context).pushNamed(
+    Navigator.of(context)
+        .pushNamed(
       MealDetailScreen.routeName,
-      arguments:id,
-    );
+      arguments: id,
+    )
+        .then((result) {
+          if (result!=null){
+            removeItem(result);
+          }
+    });
   }
 
   @override
